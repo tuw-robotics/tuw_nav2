bdcfe6bc710553460d5791c8878c8fddf6506cca
Markus Bader <markus.bader@tuwien.ac.at>
2023-08-24 13:35:30 +0200

    more config options

--- launch/nav2/nav2_launch.py
+++ launch/nav2/nav2_launch.py
@@ -31,68 +31,18 @@ def generate_launch_description():
     # Get the launch directory
     this_pgk_dir = get_package_share_directory('tuw_nav2')
 
-    namespace = LaunchConfiguration('namespace')
     use_sim_time = LaunchConfiguration('use_sim_time')
     autostart = LaunchConfiguration('autostart')
-    params_file = LaunchConfiguration('params_file')
     log_level = LaunchConfiguration('log_level')
-
-    bt_navigator_yaml          = os.path.join(this_pgk_dir, 'config', 'nav2', 'pioneer3dx', 'v1', 'bt_navigator.yaml')
      
 
-    lifecycle_nodes = ['controller_server',
-                       'smoother_server',
-                       'planner_server',
-                       'behavior_server',
-                       'bt_navigator',
-                       'waypoint_follower',
-                       'velocity_smoother']
-
-    remappings = [('/tf', 'tf'),
-                  ('/tf_static', 'tf_static')]
-
-    # Create our own temporary YAML files that include substitutions
-    param_substitutions = {
-        'use_sim_time': use_sim_time,
-        'autostart': autostart}
-
-    bt_navigator_yaml_rewritten = RewrittenYaml(
-            source_file=bt_navigator_yaml,
-            root_key=namespace,
-            param_rewrites=param_substitutions,
-            convert_types=True)
-
-
-    declare_controller_server_yaml = DeclareLaunchArgument(
-        'controller_server_yaml',
-        default_value='controller_server_purepursuite.yaml',
-        description='controller server file name')
-
-
-    declare_smoother_server_yaml = DeclareLaunchArgument(
-        'smoother_server_yaml',
-        default_value='smoother_server.yaml',
-        description='smoother server file name')
-
-    declare_behavior_server_yaml = DeclareLaunchArgument(
-        'behavior_server_yaml',
-        default_value='behavior_server.yaml',
-        description='behavior server file name')
-    
-    declare_waypoint_follower_yaml = DeclareLaunchArgument(
-        'waypoint_follower_yaml',
-        default_value='waypoint_follower.yaml',
-        description='waypoint follower file name')
-
-    declare_planner_server_yaml = DeclareLaunchArgument(
-        'planner_server_yaml',
-        default_value='planner_server.yaml',
-        description='planner server file name')
-    
-    declare_velocity_smoother_yaml = DeclareLaunchArgument(
-        'velocity_smoother_yaml',
-        default_value='velocity_smoother.yaml',
-        description='velocity smoother file name')
+    declare_controller_server_yaml = DeclareLaunchArgument( 'controller_server_yaml',  default_value='controller_server_purepursuite.yaml')
+    declare_bt_navigator_yaml      = DeclareLaunchArgument( 'bt_navigator_yaml',       default_value='bt_navigator.yaml')
+    declare_smoother_server_yaml   = DeclareLaunchArgument( 'smoother_server_yaml',    default_value='smoother_server.yaml')
+    declare_behavior_server_yaml   = DeclareLaunchArgument( 'behavior_server_yaml',    default_value='behavior_server.yaml')
+    declare_waypoint_follower_yaml = DeclareLaunchArgument( 'waypoint_follower_yaml',  default_value='waypoint_follower.yaml')
+    declare_planner_server_yaml    = DeclareLaunchArgument( 'planner_server_yaml',     default_value='planner_server.yaml')    
+    declare_velocity_smoother_yaml = DeclareLaunchArgument( 'velocity_smoother_yaml',  default_value='velocity_smoother.yaml')
     
     declare_use_robot = DeclareLaunchArgument(
         'use_robot',
@@ -106,11 +56,6 @@ def generate_launch_description():
 
     stdout_linebuf_envvar = SetEnvironmentVariable(
         'RCUTILS_LOGGING_BUFFERED_STREAM', '1')
-
-    declare_namespace_cmd = DeclareLaunchArgument(
-        'namespace',
-        default_value='',
-        description='Top-level namespace')
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         'use_sim_time',
@@ -133,6 +78,13 @@ def generate_launch_description():
             context.launch_configurations['use_version'],
             context.launch_configurations['controller_server_yaml'])
         print(controller_server_param_file_path)
+        bt_navigator_yaml_param_file_path = os.path.join(
+            this_pgk_dir,
+            'config', 'nav2',
+            context.launch_configurations['use_robot'],
+            context.launch_configurations['use_version'],
+            context.launch_configurations['bt_navigator_yaml'])
+        print(bt_navigator_yaml_param_file_path)
         behavior_server_param_file_path = os.path.join(
             this_pgk_dir,
             'config', 'nav2',
@@ -169,6 +121,7 @@ def generate_launch_description():
             context.launch_configurations['velocity_smoother_yaml'])
         print(velocity_smoother_param_file_path)
         return [SetLaunchConfiguration('controller_server_param_file_path', controller_server_param_file_path),
+                SetLaunchConfiguration('bt_navigator_yaml_param_file_path', bt_navigator_yaml_param_file_path),
                 SetLaunchConfiguration('behavior_server_param_file_path', behavior_server_param_file_path),
                 SetLaunchConfiguration('smoother_server_param_file_path', smoother_server_param_file_path),
                 SetLaunchConfiguration('waypoint_follower_param_file_path', waypoint_follower_param_file_path),
@@ -178,6 +131,17 @@ def generate_launch_description():
     create_full_path_configurations_arg = OpaqueFunction(function=create_full_path_configurations)
 
 
+    lifecycle_nodes = ['controller_server',
+                       'smoother_server',
+                       'planner_server',
+                       'behavior_server',
+                       'bt_navigator',
+                       'waypoint_follower',
+                       'velocity_smoother']
+
+    remappings = [('/tf', 'tf'),
+                  ('/tf_static', 'tf_static')]
+
     load_nodes = GroupAction(
         actions=[
             Node(
@@ -185,7 +149,8 @@ def generate_launch_description():
                 executable='controller_server',
                 output='screen',
                 respawn_delay=2.0,
-                parameters=[LaunchConfiguration('controller_server_param_file_path')],
+                parameters=[LaunchConfiguration('controller_server_param_file_path'),
+                            {'use_sim_time': use_sim_time}],
                 arguments=['--ros-args', '--log-level', log_level],
                 remappings=remappings + [('cmd_vel', 'cmd_vel_nav')]),
             Node(
@@ -194,7 +159,8 @@ def generate_launch_description():
                 name='smoother_server',
                 output='screen',
                 respawn_delay=2.0,
-                parameters=[LaunchConfiguration('smoother_server_param_file_path')],
+                parameters=[LaunchConfiguration('smoother_server_param_file_path'),
+                            {'use_sim_time': use_sim_time}],
                 arguments=['--ros-args', '--log-level', log_level],
                 remappings=remappings),
             Node(
@@ -203,7 +169,8 @@ def generate_launch_description():
                 name='planner_server',
                 output='screen',
                 respawn_delay=2.0,
-                parameters=[LaunchConfiguration('planner_server_param_file_path')],
+                parameters=[LaunchConfiguration('planner_server_param_file_path'),
+                            {'use_sim_time': use_sim_time}],
                 arguments=['--ros-args', '--log-level', log_level],
                 remappings=remappings),
             Node(
@@ -212,7 +179,8 @@ def generate_launch_description():
                 name='behavior_server',
                 output='screen',
                 respawn_delay=2.0,
-                parameters=[LaunchConfiguration('behavior_server_param_file_path')],
+                parameters=[LaunchConfiguration('behavior_server_param_file_path'),
+                            {'use_sim_time': use_sim_time}],
                 arguments=['--ros-args', '--log-level', log_level],
                 remappings=remappings),
             Node(
@@ -221,7 +189,9 @@ def generate_launch_description():
                 name='bt_navigator',
                 output='screen',
                 respawn_delay=2.0,
-                parameters=[bt_navigator_yaml_rewritten],
+                parameters=[LaunchConfiguration('bt_navigator_yaml_param_file_path'),
+                            {'use_sim_time': use_sim_time,
+                             'autostart': autostart}],
                 arguments=['--ros-args', '--log-level', log_level],
                 remappings=remappings),
             Node(
@@ -230,7 +200,8 @@ def generate_launch_description():
                 name='waypoint_follower',
                 output='screen',
                 respawn_delay=2.0,
-                parameters=[LaunchConfiguration('waypoint_follower_param_file_path')],
+                parameters=[LaunchConfiguration('waypoint_follower_param_file_path'),
+                            {'use_sim_time': use_sim_time}],
                 arguments=['--ros-args', '--log-level', log_level],
                 remappings=remappings),
             Node(
@@ -239,7 +210,8 @@ def generate_launch_description():
                 name='velocity_smoother',
                 output='screen',
                 respawn_delay=2.0,
-                parameters=[LaunchConfiguration('velocity_smoother_param_file_path')],
+                parameters=[LaunchConfiguration('velocity_smoother_param_file_path'),
+                            {'use_sim_time': use_sim_time}],
                 arguments=['--ros-args', '--log-level', log_level],
                 remappings=remappings +
                         [('cmd_vel', 'cmd_vel_nav'), ('cmd_vel_smoothed', 'cmd_vel')]),
@@ -264,9 +236,9 @@ def generate_launch_description():
     # Declare the launch options
     ld.add_action(declare_use_robot)
     ld.add_action(declare_use_version)
-    ld.add_action(declare_namespace_cmd)
     ld.add_action(declare_use_sim_time_cmd)
     ld.add_action(declare_controller_server_yaml)
+    ld.add_action(declare_bt_navigator_yaml)
     ld.add_action(declare_behavior_server_yaml)
     ld.add_action(declare_smoother_server_yaml)
     ld.add_action(declare_waypoint_follower_yaml)
