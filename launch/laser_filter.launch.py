1eda339e6f60719697a7cc8d52be7edbe3f47bd9
Markus Bader <markus.bader@tuwien.ac.at>
2023-09-13 14:47:32 +0200

    config works now also for stage

--- launch/laser_filter.launch.py
+++ launch/laser_filter.launch.py
@@ -1,6 +1,8 @@
+
+import os
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, SetLaunchConfiguration
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
@@ -10,35 +12,58 @@ def generate_launch_description():
     this_pgk = 'tuw_nav2'
     this_pgk_dir = get_package_share_directory(this_pgk)
 
-    use_sim_time = LaunchConfiguration('use_sim_time')
-    scan_src = LaunchConfiguration('scan_src')
-    scan_des = LaunchConfiguration('scan_des')
+    use_robot = LaunchConfiguration('use_robot')
+    declare_use_robot_cmd = DeclareLaunchArgument(
+        'use_robot',
+        default_value='pioneer3dx',
+        description='Robot used and configuration folder used: ./nav2/$use_robot/$use_version/..')
 
+    use_sim_time = LaunchConfiguration('use_sim_time')
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         'use_sim_time',
         default_value='false',
         description='Use simulation (Gazebo) clock if true')
     
+    scan_src = LaunchConfiguration('scan_src')
     declare_scan_src_cmd = DeclareLaunchArgument(
         'scan_src',
         default_value='scan_raw',
         description='laser scan source')
     
+    scan_des = LaunchConfiguration('scan_des')
     declare_scan_des_cmd = DeclareLaunchArgument(
         'scan_des',
         default_value='scan',
         description='laser scan destination')
     
+    filter_yaml = LaunchConfiguration('filter_yaml')
+    declare_filter_yaml_cmd  = DeclareLaunchArgument( 
+        'filter_yaml',  
+        default_value='shadow_filter.yaml',
+        description='filter configuration')
+
+    def create_full_path_configurations(context):
+        filter_file_path = os.path.join(
+            this_pgk_dir,
+            'config', 'laser_filter',
+            context.launch_configurations['use_robot'],
+            context.launch_configurations['filter_yaml'])
+        print(filter_file_path)
+        return [SetLaunchConfiguration('filter_file_path', filter_file_path)]
+
+    create_full_path_configurations_arg = OpaqueFunction(function=create_full_path_configurations)
+
     return LaunchDescription([
         declare_use_sim_time_cmd, 
+        declare_use_robot_cmd,
+        declare_filter_yaml_cmd,
         declare_scan_src_cmd, 
         declare_scan_des_cmd, 
+        create_full_path_configurations_arg,
         Node(
             package="laser_filters",
             executable="scan_to_scan_filter_chain",
-            parameters=[PathJoinSubstitution([
-                    this_pgk_dir, "config/laser_filter/p3dx", "shadow_filter_example.yaml",
-                ])],
+            parameters=[LaunchConfiguration('filter_file_path')],
             remappings=[
                 ("scan", scan_src),
                 ("scan_filtered", scan_des),]
